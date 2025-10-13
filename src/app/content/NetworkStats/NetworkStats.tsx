75053e2e38fbc2fee0f443240f7ec57641a6a235
David Leung <265220+dhl@users.noreply.github.com>
2025-10-14 00:39:51 +0800

    Use regular font for validator StatCard

--- src/app/content/NetworkStats/NetworkStats.tsx
+++ src/app/content/NetworkStats/NetworkStats.tsx
@@ -177,7 +177,7 @@ export default function NetworkStats() {
       <div className="wrapper !px-0">
         <div className="grid grid-cols-2 xl:grid-cols-4 xl:gap-x-6 divide-x divide-border xl:divide-x-0 xl:divide-y-0 relative">
           <div className="absolute h-px w-full bg-border z-10 top-1/2 -translate-y-1/2 xl:hidden"></div>
-          <StatCard title={t("ui.validator")}>
+          <StatCard title={t("ui.validator")} useMonospace={false}>
             <div className="absolute w-full xl:w-[calc(100%+24px)] h-[calc(100%+1px)] left-0 -top-[1px] overflow-hidden pointer-events-none">
               <div className="absolute w-[1350px] h-[500px] mix-blend-color-dodge top-1/3 -translate-y-3/5 left-1/2 -translate-x-1/2">
                 <Image
@@ -286,17 +286,24 @@ const StatCard = ({
   title,
   children,
   className,
+  useMonospace = true,
 }: {
   title: string;
   children: ReactNode;
   className?: string;
+  useMonospace?: boolean;
 }) => {
   return (
     <div className={classNames("group/stat relative", className)}>
       <div className="p-5 xl:p-6 flex flex-col gap-y-3">
         <div className="hidden xl:block group-last-of-type/stat:hidden absolute h-[calc(100%+16px)] -right-6 -top-[8px] w-px bg-border z-10"></div>
         <div className="relative z-20 font-medium text-tertiary">{title}</div>
-        <div className="flex items-center gap-x-2.5 text-lg sm:text-2xl xl:text-[26px] leading-[100%] font-medium font-mono text-primary">
+        <div
+          className={classNames(
+            "flex items-center gap-x-2.5 text-lg sm:text-2xl xl:text-[26px] leading-[100%] font-medium text-primary",
+            useMonospace && "font-mono"
+          )}
+        >
           {children}
         </div>
       </div>
